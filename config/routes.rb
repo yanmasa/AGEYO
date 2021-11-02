729625db5ff1e4995c93ac7fcf09d6d09111bdd1
yanmasa <ueda3ban@gmail.com>
2021-11-02 07:18:29 +0000

    devise 2種類のユーザーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :recipients, skip: [:registrations,:passwords], controllers: {
+    sessions: 'recipient/sessions'
+  }
+  devise_for :contributors, skip: [:passwords], controllers: {
+    registrations: 'contributor/registrations',
+    sessions: 'contributor/sessions'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'homes#top'
 end
