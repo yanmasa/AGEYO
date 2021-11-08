cf39cf967dc7620664edba4fe9e324d972862295
yanmasa <ueda3ban@gmail.com>
2021-11-08 05:07:37 +0000

    recipient側のcontributorの作成

--- app/controllers/recipient/contributors_controller.rb
+++ app/controllers/recipient/contributors_controller.rb
@@ -1,5 +1,6 @@
 class Recipient::ContributorsController < ApplicationController
   def show
     @contributor = Contributor.find(params[:id])
+    @posts = @contributor.posts.where(status: '公開')
   end
 end
