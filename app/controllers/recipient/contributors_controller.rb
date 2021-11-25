5f991b6b0ab5ef4a1a7e7950cb57b7f8134a6370
yanmasa <ueda3ban@gmail.com>
2021-11-25 08:06:14 +0000

    ビューの編集

--- app/controllers/recipient/contributors_controller.rb
+++ app/controllers/recipient/contributors_controller.rb
@@ -2,6 +2,6 @@ class Recipient::ContributorsController < ApplicationController
   def show
     @contributor = Contributor.find(params[:id])
     @recipient = current_recipient
-    @posts = @contributor.posts.where(status: '公開')
+    @posts = @contributor.posts.where(status: '公開').order(updated_at: :desc).page(params[:page]).per(15)
   end
 end
